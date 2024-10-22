3b1df2da32ba7a317849065233c260d8cbc6fb0b
Cesar Oswaldo Bernal Sanchez <cesar.bernal.ti@gmail.com>
2024-10-22 16:49:31 -0700

    Se agregar CrossOrigin a ProveedorController

--- src/main/java/com/backed/sistema_abarrotes_api/controller/ProveedorController.java
+++ src/main/java/com/backed/sistema_abarrotes_api/controller/ProveedorController.java
@@ -5,16 +5,14 @@ import com.backed.sistema_abarrotes_api.service.ProveedorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/proveedores")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class ProveedorController {
 
     @Autowired
